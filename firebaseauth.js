aeb55680a6b6d4ca74b9f43c64ce21d31ee02e46
Mikkrok1020 <mkkrawczyk10@gmail.com>
2024-10-07 20:37:05 +0200

    zmiana

--- firebaseauth.js
+++ firebaseauth.js
@@ -1,5 +1,7 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+  import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js"
+  import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js"
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
@@ -20,3 +22,14 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  const signUp=document.getElementById('submitSignUp')
+  signUp.addEventListener('click', (event)=>{
+    event.preventDefault();
+    const email=document.getElementById('remail').value;
+    const password=document.getElementById('rpassword').value;
+    const name=document.getElementById('rname').value;
+    const username=document.getElementById('rusername').value;
+    
+    const auth=getAuth();
+    
+  })
