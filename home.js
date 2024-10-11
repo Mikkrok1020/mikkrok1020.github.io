30efbf62fdb4487a9007ce32dd1a071348307518
Mikkrok1020 <mkkrawczyk10@gmail.com>
2024-10-11 19:50:41 +0200

    Create home.js

---
+++ home.js
@@ -0,0 +1,47 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import {getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js"
+import {getFirestore, getDoc, doc} from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js"
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA-sUt1u7uK1GN_x4XkmDgTzunzNHR1N3U",
+    authDomain: "register-and-login-61c50.firebaseapp.com",
+    databaseURL: "https://register-and-login-61c50-default-rtdb.firebaseio.com",
+    projectId: "register-and-login-61c50",
+    storageBucket: "register-and-login-61c50.appspot.com",
+    messagingSenderId: "248872359218",
+    appId: "1:248872359218:web:7fdd5b2442b58498cb732b",
+    measurementId: "G-T6MGC522FC"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  const auth=getAuth();
+  const db=getFirestore();
+
+  onAuthStateChanged(aut, (user)=>{
+    const loggedInUserId=localStorage.getItem('loggedInUserId');
+    if(loggedInUserId){
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+        .then((docSnap)=>{
+            if(docSnap.exists()){
+                const userData=docSnap.data();
+                document.getElementById('loggedUserUsername').innerText=userData.username;
+                document.getElementById('loggedUserEmail').innerText=userData.email;
+                document.getElementById('loggedUserName').innerText=userData.name;
+            }
+            else{
+                console.log("no document found matching id")
+            }
+        })
+        .catch((error)=>{
+            console.log("Error getting document ");
+        })
+    }
+    else{
+        console.log("User Id not found in local storage.")
+    }
+  })
